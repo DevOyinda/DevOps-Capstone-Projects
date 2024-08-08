7de904a127fe4e615c09fab144717c2f23525d00
DevOyinda <kudiratoyindamolad@gmail.com>
2024-08-08 18:07:51 -0400

    Made changes to file by adding comments

--- capstone_proj_3/multiple.sh
+++ capstone_proj_3/multiple.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Prompt the user to enter a number for the multiplication table
+# Step 1: Prompt the user to enter a number for the multiplication table
 read -p "Enter a number for the multiplication table: " num
 
 # To allow only numbers/integers
@@ -18,24 +18,25 @@ else
 
 fi
 
-# Prompt the user to choose between a full or partial table
+# Step 2: Prompt the user to choose between a full or partial table
 
 read -p "Do you want a full table or a partial table? (Enter 'f' for full, 'p' for partial): " table
 
-# Check if the user chose a partial table
+# Step 3: Check if the user chose a partial table
 if [ "$table" = "p" ]; then
 	read -p "Enter the starting number (between 1 and 10): " start
 
 	read -p "Enter the ending number (between 1 and 10): " end
 
 
-	# Check if the input is valid
+	# Step 4: Check if the input is valid
 	if ((start < 1 || start > 10 || end < 1 || end > 10 || start > end)); then 
 		echo "Invalid input. Please enter numbers between 1 and 10, and ensure that the starting number is less than or equal to the ending number."
 		echo "Showing full table instead."
 		
 		
 		#Print full multiplication table of the initial number entered
+		# using list loop
 		for i in {1..10}; do
 			echo "$num x $i = $((num * i))"
                 done
@@ -43,15 +44,17 @@ if [ "$table" = "p" ]; then
 	fi
 
 	
-	# To display partial multiplication table
+	# Step 5: To display partial multiplication table
 	echo "The partial multiplication table for $num from $start to $end:"
 	
+	# using C-style loop 
 	for ((i = start; i <= end; i++)); do
+		# prints multiplication table within range
 		echo "$num x $i = $((num * i))"
 	done
 
 
-# Check if user chose full table
+# Step 6: Check if user chose full table
 elif [ "$table" = "f" ]; then 
 	echo "The multiplication table for $num:"
 
